3a9f0da687dc7e38b79a7d4bcd2d41f805ef7848
Paras Arya <parasarya289@gmail.com>
2024-07-25 21:49:59 +0530

    feat: api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import { Pool } from "@neondatabase/serverless";
+import { cookies } from "next/headers";
 
 export async function POST(request: Request) {
   let { email, password } = await request.json();
@@ -12,23 +13,19 @@ export async function POST(request: Request) {
       [email]
     );
     if (!result.rows[0]) {
+      return Response.json({ message: "Invalid Email" }, { status: 404 });
+    }
+    if (result.rows[0].password !== password) {
+      return Response.json({ message: "Invalid Password" }, { status: 404 });
+    }
+    if (result.rows[0].isverified === false) {
       return Response.json(
-        { message: "Invalid Email"},
-        { status: 404 }
+        { message: "Please verify your email" },
+        { status: 401 }
       );
     }
-    if(result.rows[0].password !== password){
-        return Response.json(
-            { message: "Invalid Password"},
-            { status: 404 }
-          );
-    }
-    if(result.rows[0].isverified === false){
-        return Response.json(
-            { message: "Please verify your email"},
-            { status: 401 }
-          );
-    }
+    cookies().set("session", result.rows[0]);
+    // console.log(cookies().get('session'))
     return Response.json(
       { message: "Logged in", user: result.rows[0] },
       { status: 200 }
