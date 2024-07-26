a5c9202d7827dd5746a3960171162bff2c24e816
Paras Arya <parasarya289@gmail.com>
2024-07-26 05:55:04 +0530

    removed

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -4,46 +4,25 @@ import "../../../styles/auth.css";
 import SignUp from "~/components/Signup/Signup";
 import Login from "~/components/Login/Login";
 import { useAuth } from "~/context/authContext";
-import { useRouter } from "next/router";
 import { redirect } from "next/navigation";
 
 const Auth = () => {
   const [authType, setAuthType] = useState("signup");
-  const [checkOtp, setCheckOtp] = useState(false);
-  const [generatedOtp, setGeneratedOtp] = useState(0);
-  const [newUser, setNewUser] = useState({});
   const { user } = useAuth();
   useEffect(() => {
     if (user?.fullname) {
       redirect("/");
     }
   }, [user]);
-  const onOtpSubmit = (userOtp) => {
-    // if (userOtp == generatedOtp) {
-    //   createUser(newUser);
-    //   navigate("/");
-    // } else {
-    //   alert("Wrong OTP");
-    // }
-  };
   return (
     <div className="auth">
       {!user?.name ? (
         <>
           {authType === "login" ? (
             <Login setAuthType={setAuthType} />
-          ) : checkOtp ? (
-            <Otp digit={8} onOtpSubmit={onOtpSubmit} />
           ) : (
-            <SignUp
-              setAuthType={setAuthType}
-              setCheckOtp={setCheckOtp}
-              createOTP={() => {}}
-              setGeneratedOtp={setGeneratedOtp}
-              setNewUser={setNewUser}
-            />
+            <SignUp setAuthType={setAuthType}/>
           )}
-          {checkOtp && generatedOtp}
         </>
       ) : (
         <>
