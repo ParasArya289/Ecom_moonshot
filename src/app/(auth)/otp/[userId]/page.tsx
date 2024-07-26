17270a6e2367bd9e309864d2821d5f8ba0baf23e
Paras Arya <parasarya289@gmail.com>
2024-07-26 05:54:53 +0530

    pages

---
+++ src/app/(auth)/otp/[userId]/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import "../../../../styles/otp.css";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { useAuth } from "~/context/authContext";
+
+const Otp = ({ digit = 6 }) => {
+  const params = useParams();
+  const [otp, setOtp] = useState(new Array(digit).fill(""));
+  const [verificationLoading, setVerificationLoading] = useState(false);
+  const { user,setUser } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (user?.fullname) {
+      router.replace("/");
+    }
+  }, []);
+
+  const inputRefs = useRef([]);
+  const formRef = useRef(null);
+
+  const handleOtpSubmit = async (e) => {
+    e.preventDefault();
+    setVerificationLoading(true);
+    try {
+      console.log(params.userId, parseInt(otp.join("")));
+      const res = await axios.post("/api/verify-user", {
+        id: params.userId,
+        otp: parseInt(otp.join("")),
+      });
+      setUser(res.data.user);
+      router.replace("/");
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setVerificationLoading(false);
+    }
+  };
+  const handleChange = (e, i) => {
+    const value = e.target.value;
+    const newOtp = [...otp];
+    newOtp[i] = value.substring(value.length - 1);
+    setOtp(newOtp);
+  };
+  const handleKeyDown = (e, i) => {
+    const keyPressed = e.key;
+    const inputValue = e.target.value;
+    if (e.key && e.key !== "Backspace" && e.key !== "Enter") {
+      const inputValue = e.target.value;
+      if (inputValue.length === 1 && inputRefs.current[i + 1]) {
+        inputRefs.current[i + 1].focus();
+      }
+    }
+    // if (keyPressed === "Backspace") {
+    //   if (!inputValue && i < digit && inputRefs.current[i - 1]) {
+    //     inputRefs.current[i - 1].focus();
+    //   } else {
+    //     inputRefs.current[i].value = "";
+    //   }
+    // }
+  };
+  return (
+    <div className="otp">
+      <h4>Verify your OTP</h4>
+      <h5>Enter the {digit} digit OTP</h5>
+      <form ref={formRef} onSubmit={handleOtpSubmit}>
+        <div className="input__container">
+          {otp.map((val, i) => (
+            <input
+              disabled={verificationLoading}
+              type="text"
+              ref={(input) => (inputRefs.current[i] = input)}
+              key={i}
+              name={i}
+              value={val}
+              autoFocus={i === 0}
+              onChange={(e) => handleChange(e, i)}
+              onKeyDown={(e) => handleKeyDown(e, i)}
+              required
+            />
+          ))}
+        </div>
+        <button disabled={verificationLoading} type="submit">
+          {verificationLoading ? "Loading..." : "VERIFY"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Otp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,105 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import "../styles/home.css";
 import { redirect } from "next/navigation";
 import { useAuth } from "~/context/authContext";
 import { getSession } from "~/lib/lib";
+import axios from "axios";
 
 const Home = () => {
   const [page, setPage] = useState(1);
   const [perPageData, setPerPageData] = useState(page * 6);
-  const {user} = useAuth();
-useEffect(()=>{
-  if(!user){
-    redirect("/auth")
-  }
-},[])
+  const [userInterests, setUserInterests] = useState([]);
+  const [interestLoading, setInterestLoading] = useState(false);
+  const { user } = useAuth();
+  useEffect(() => {
+    if (!user) {
+      redirect("/auth");
+    }
+  }, []);
+  useEffect(() => {
+    const fetchUserInterests = async () => {
+      // setInterestLoading(true);
+      try {
+        const res = await axios.get(`/api/interests?user_id=${user.id}`);
+        setUserInterests(res.data.interests);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        // setInterestLoading(false);
+      }
+    };
+    fetchUserInterests();
+  }, []);
   let startingIndex = page * 6 - 6;
   let endingIndex = page * 6;
 
-//   const pageLimit = Math.ceil(user?.interests?.length / 6);
+  const pageLimit = Math.ceil(userInterests?.length / 6);
 
-//   const handleCheckedInteres = (name) => {
-//     updateInterests(name);
-//   };
+  const handleCheckedInterest = async (category_id, is_interest) => {
+    setInterestLoading(true);
+    try {
+      const res = await axios.post(`api/change-interests`, {
+        user_id: user.id,
+        category_id,
+        is_interest: !is_interest,
+      });
+      setUserInterests(res.data.interests);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setInterestLoading(false);
+    }
+  };
 
-//   const handleNextPage = (e, page = 1) => {
-//     const value = e.target?.value;
-//     console.log(value);
-//     if (value === "10inc") {
-//       if (page + 10 <= pageLimit) {
-//         setPage((p) => p + 10);
-//         // setPage(pageLimit);
-//         return;
-//       } else {
-//         return;
-//       }
-//     }
-//     if (value === "10dec") {
-//       if (page >= 10) {
-//         setPage(page - 10);
-//         return;
-//       }
-//     }
-//     setPage(page);
-//   };
+  const handleNextPage = (e, page = 1) => {
+    const value = e.target?.value;
+    if (value === "10inc") {
+      if (page + 10 <= pageLimit) {
+        setPage((p) => p + 10);
+        // setPage(pageLimit);
+        return;
+      } else {
+        return;
+      }
+    }
+    if (value === "10dec") {
+      if (page >= 10) {
+        setPage(page - 10);
+        return;
+      }
+    }
+    setPage(page);
+  };
 
   return (
     <div className="home">
       <h4>Please Mark your interests</h4>
       <h5>We will keep you notified</h5>
       <div className="checkboxes">
-        {/* {user?.interests?.slice(startingIndex, endingIndex).map((interest) => (
+        {userInterests?.slice(startingIndex, endingIndex).map((interest) => (
           <label key={interest.name}>
             <input
               type="checkbox"
-              checked={interest.isChecked}
-              onChange={()=>handleCheckedInteres(interest.name)}
+              checked={interest.is_interest}
+              disabled={interestLoading}
+              onChange={() =>
+                handleCheckedInterest(
+                  interest.category_id,
+                  interest.is_interest
+                )
+              }
             />
             {interest.name}
           </label>
-        ))} */}
+        ))}
       </div>
 
       <div className="page__controllers">
-        {/* <button value="10dec" onClick={(e) => handleNextPage(e)}>
+        <button value="10dec" onClick={(e) => handleNextPage(e)}>
           {"<"}
-        </button> */}
-        {/* {new Array(6).fill(0).map((_, i) => {
+        </button>
+        {new Array(6).fill(0).map((_, i) => {
           if (page + i <= pageLimit) {
             return (
               <button
@@ -77,10 +111,10 @@ useEffect(()=>{
               </button>
             );
           }
-        })} */}
-        {/* <button value="10inc" onClick={(e) => handleNextPage(e)}>
+        })}
+        <button value="10inc" onClick={(e) => handleNextPage(e)}>
           {">"}
-        </button> */}
+        </button>
       </div>
     </div>
   );
